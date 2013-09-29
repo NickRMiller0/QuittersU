1eafaadcfb3f90502babf6af915aab5e99574c62
Nick Miller <nicholas.raymond.miller@gmail.com>
2013-09-29 14:50:41 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuittersU::Application.config.secret_token = '65c7cafbed8fa34c020a82d6506f1fb8e62cbfcec00f2e4ecf9d71563ef1c1028384aa30b5eaa1933532f11e9a855863387dbed80216294f4b9e851011bb4a83'
