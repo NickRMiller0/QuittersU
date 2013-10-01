5b144601eb3b9a356bd90dc4cdad0858526aa1ec
Nick Miller <nickm6980@gmail.com>
2013-10-01 06:42:02 -0400

    Add jQuery Masonry

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,9 +4,10 @@ class Pin < ActiveRecord::Base
   validates :description, presence: true
   validates :user_id, presence: true
   has_attached_file :image, styles: { medium: "320x240>"}
+  
   validates_attachment :image, presence: true,
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                             size: { less_than: 5.megabytes }
   belongs_to :user
-  has_attached_file :image, styles: {medium: "320x240>"}
+  
 end
