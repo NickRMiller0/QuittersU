58697e94158d4c9e1cd98cdc6d0d1c34c8c0b625
Nick Miller <nickm6980@gmail.com>
2014-06-23 21:37:12 -0400

    Updated PostgreSQL in Prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,20 +70,5 @@ QuittersU::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   #In production, :host should be set to the actual host of your application.
-  configure :development do
-  set :database, "sqlite3:///database.db"
-end
 
-configure :production do
-  db = URI.parse(ENV['HEROKU_POSTGRESQL_AQUA_URL'] || 'postgres://localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
-end
 end
