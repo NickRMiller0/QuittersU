3b29de9bad42d577689fe86f48aac36de8f0fba5
Nick Miller <nickm6980@gmail.com>
2014-06-23 21:35:23 -0400

    Updated PostgreSQL in Prod

--- config/environment.rb
+++ config/environment.rb
@@ -4,19 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 QuittersU::Application.initialize!
 
-configure :development do
-  set :database, "sqlite3:///database.db"
-end
-
-configure :production do
-  db = URI.parse(ENV['HEROKU_POSTGRESQL_AQUA_URL'] || 'postgres://localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
-end
