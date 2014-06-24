8f089be2b503ab0b74e4599ef59c9026777ad740
Nick Miller <nickm6980@gmail.com>
2014-06-23 21:10:22 -0400

    updated environment.rb for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ configure :development do
 end
 
 configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  db = URI.parse(ENV['HEROKU_POSTGRESQL_AQUA_URL'] || 'postgres://localhost/mydb')
 
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
