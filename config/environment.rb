6f8d0cc1eb0fd92aa911ad65ffa2271057525b67
Nick Miller <nickm6980@gmail.com>
2014-06-23 21:01:20 -0400

    updated environment.rb for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,20 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 QuittersU::Application.initialize!
+
+configure :development do
+  set :database, "sqlite3:///database.db"
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
